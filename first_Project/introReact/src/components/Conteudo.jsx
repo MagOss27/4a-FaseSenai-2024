d88583fa49ab74962e7866ca1ad7853c98dc596c
MAGNUS OSS <magnus_oss@estudante.sesisenai.org.br>
2024-08-06 17:15:20 -0300

    Criação do first_Project

--- first_Project/introReact/src/components/Conteudo.jsx
+++ first_Project/introReact/src/components/Conteudo.jsx
@@ -1,16 +1,22 @@
 import './Conteudo.css';
 
 function Conteudo() {
-
     return (
         <div className="conteudo-container">
-            <p>Taylor Swift é cantora e compositora e uma das mulheres mais bem sucedidas do mundo da música.
-                Nascida em Reading na Pensilvânia, se mudou aos 13 anos para Nashville, no Tennessee.
-                Aos 14, Taylor se tornou a artista mais jovem a fechar um contrato de composição com a Sony Music.
+
+            <p>Taylor Alison Swift é uma cantora, compositora, instrumentista, produtora musical e atriz nascida em Reading, Pensilvânia, Estados Unidos. Nasceu a 13 de dezembro de 1989, filha de Scott Swift, corretor da bolsa de valores, e Andrea Swift, dona de casa, e tem um irmão mais novo, Austin Swift. Taylor Swift cresceu numa quinta de cultivo de árvores de Natal e, desde muito cedo, se interessou pela música.</p>
+            <img className='imagem-conteudo' src='./images/taylorSwift02.jfif' alt='Taylor Swift' />
+            <p>
+                Taylor começou a demonstrar seu talento musical em concursos de karaoke locais. Apesar de muito jovem, a sua presença forte e inclinação para a música chamaram a atenção da indústria musical. Com apenas 14 anos mudou-se com sua família para Nashville, a cidade que é o coração da música country, para seguir o seu sonho.
+            </p>
+            <p>
+                Em 2006, e com apenas 16 anos, Taylor lançou o seu álbum de estreia homónimo. Foi um sucesso imediato, com 5 milhões de cópias vendidas nos Estados Unidos, certificado cinco vezes como disco de platina.
+            </p>
+            <p>
+                O single "Tim McGraw" foi um sucesso que a lançou para a atenção do público e da indústria musical, e estabeleceu a sua reputação como uma promissora artista country. Taylor Swift recebeu, em 2007, o prémio Horizon Award (Revelação) no CMA Awards, os Grammys da música country.
             </p>
         </div>
-    )
+    );
 }
 
-export default Conteudo
-
+export default Conteudo;
